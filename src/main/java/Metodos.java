ca1a83110cc4bb15fd2c629f28e3983330912c14
Víctor Mardones <v.mardones04@ufromail.cl>
2020-05-27 08:53:40 -0400

    Corregido el método validarRut

--- src/main/java/Metodos.java
+++ src/main/java/Metodos.java
@@ -201,13 +201,13 @@ public class Metodos {
         rut = rut.replace("-", "");
         int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
 
-        char dv = rut.charAt(rut.length() - 1);
+        char digitoVerificador = rut.charAt(rut.length() - 1);
 
         int m = 0, s = 1;
         for (; rutAux != 0; rutAux /= 10) {
             s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
         }
-        if (dv == (char) (s != 0 ? s + 47 : 75)) {
+        if (digitoVerificador == (char) (s != 0 ? s + 47 : 75)) {
             validacion = true;
         }
 
